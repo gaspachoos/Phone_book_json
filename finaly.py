cf1a3114a1a42ebc66617f38e1b0a620795682e0
Garik <gar.minasyan.97@mail.ru>
2024-04-03 18:59:53 +0400

    Внедрили первую позицию выбора,программа сразу запрашивает имя для данного контакта,дальше входим в цикл для номера,что дает нам возможность добавлять для 1 контакта сколько угодно номеров,когда закончили внедрять номера,переходим в стадию записи email,все данные собираем и внедраем уже в json

--- finaly.py
+++ finaly.py
@@ -14,3 +14,36 @@ while menu:
         'Выход из меню - нажмите 6 '
         )
     choice = int(input('Введите число: '))
+    if choice == 1:
+        user_info = {}
+        name = input('Введите имя: ')
+        user_info["user name"] = name
+        print('Хотите добавить номер для данного контакта?\n'
+              'Введите "да" или "нет"')
+        answ_about_number = input()
+        num_adder = True
+        user_info["user phone"] = []
+        if answ_about_number == "да":
+            while num_adder:
+                phone_num = input('Введите номер телефона: ')
+                user_info["user phone"].append(phone_num)
+                print('Хотите добавить еще номер к данному контакту?\n'
+                      'Введите "да" или "нет" ')
+                repeat_add_num = input()
+                if repeat_add_num == "да":
+                    continue
+                elif repeat_add_num == "нет":
+                    num_adder = False
+        if answ_about_number == "нет":
+            user_info["user phone"].append("пусто")
+        print('Хотите добавить email для данного контакта?\n'
+              'Введите "да" или "нет"')
+        answ_about_email = input()
+        if answ_about_email == "да":
+            email = input('Введите email: ')
+            user_info["user email"] = email
+        elif answ_about_email == "нет":
+            user_info["user email"] = "пусто"
+        contacts.append(user_info)
+        with open('users.json', 'w', encoding='utf-8') as file:
+            json.dump(contacts, file, ensure_ascii=False, indent=2)
